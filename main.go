56a55a9020377326c6178ca07748624a9815ea32
Leonard Theodorus <aoesotfs@gmail.com>
2024-08-28 10:09:25 +0700

    feat: delete command

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"regexp"
+	"strconv"
 	"strings"
-    "strconv"
 )
 
 const configFileName = ".mterm"
+const tempFilename = "tmp.mterm"
 const scriptFileName = "mterm.sh"
 
 func isHelpFlagPresent() bool {
@@ -107,11 +109,12 @@ func insertNewPath(newPath string) {
 }
 
 func readConfigFile() []string{
+
     dirErr := os.Chdir(getConfigFilePath())
     if dirErr != nil {
         //user home dir doesn't exists
         fmt.Println("Can't access directory: ", dirErr)
-        os.Exit(1)
+        os.Exit(0)
     }
 
     configFile, err := os.OpenFile(configFileName, os.O_RDONLY, 0400)
@@ -150,7 +153,7 @@ func getConfigFilePath() string {
 func getSavedPath(idx int) string {
     savedPaths := readConfigFile()
     lineNumber := getLineNumber()
-    if idx >= lineNumber {
+    if idx >= lineNumber || idx <= 0{
         fmt.Println("Path with that number doesn't exist. Run -p or --print to see saved paths")
         os.Exit(0)
     }
@@ -162,6 +165,77 @@ func getSavedPath(idx int) string {
 func jumpToPath(idx int) {
     path := getSavedPath(idx)
     fmt.Println(path)
+}
+
+func deletePath(idx int) {
+    if idx >= getLineNumber() || idx <= 0 {
+        fmt.Println("Path with that number doesn't exist. Run -p or --print to see saved paths")
+        os.Exit(0)
+    }
+
+    dirErr := os.Chdir(getConfigFilePath())
+    if dirErr != nil {
+        //user home dir doesn't exists
+        fmt.Println("Can't access directory: ", dirErr)
+        os.Exit(0)
+    }
+
+    configFile, err := os.OpenFile(configFileName, os.O_RDWR, 0644)
+    if err != nil {
+        //Opening file error || file doesn't exists
+        fmt.Println("Config doesn't exist yet, run -i or --insert to create one")
+        os.Exit(0)
+    }
+    defer configFile.Close()
+
+    tempFile, err := os.OpenFile(tempFilename, os.O_CREATE | os.O_APPEND | os.O_RDWR, 0644)
+    if err != nil {
+        fmt.Println("Error executing command: ", err)
+        os.Exit(0)
+    }
+    defer tempFile.Close()
+
+    scanner := bufio.NewScanner(configFile)
+    parsedIdx := strconv.Itoa(idx)
+    pattern := fmt.Sprintf(`%s.*`, parsedIdx)
+    re := regexp.MustCompile(pattern)
+    
+    edited := false
+    deletedPath := ""
+    for scanner.Scan() {
+        currText := string(scanner.Text())
+
+        if re.MatchString(currText) {
+            edited = true
+            deletedPath = currText
+            continue
+        }
+
+        if !edited {
+            tempFile.WriteString(currText + "\n")
+        } else {
+            splittedString := strings.Split(currText, ".")
+            lineNumber := splittedString[0]
+            path := splittedString[1]
+
+            convertedLineNumber, convErr := strconv.Atoi(lineNumber)
+            if convErr != nil {
+                fmt.Println("Error converting number: ", convErr)
+                os.Exit(0)
+            }
+            convertedLineNumber--
+
+            newLineNumber := strconv.Itoa(convertedLineNumber)
+            newContent := fmt.Sprintf("%s.%s\n", newLineNumber, path)
+            tempFile.WriteString(newContent)
+        }
+    }
+
+    moveErr := os.Rename(tempFilename, configFileName)
+    if moveErr != nil {
+        fmt.Println("Fail executing move: ", moveErr)
+    }
+    fmt.Printf("Successfully deleted path: %s\n", deletedPath)
 }
 
 func main () {
@@ -176,6 +250,7 @@ func main () {
         pathFlag := flag.String("i", "Esc", "Insert new path")
         readFlag := flag.Bool("p", false, "Print out saved paths")
         jumpFlag := flag.Int("j", 0, "Jump to saved path")
+        deleteFlag := flag.Int("d", 0, "Delete chosen path")
         for idx, arg := range os.Args {
 
             if arg == "--insert"{
@@ -226,6 +301,29 @@ func main () {
                 fmt.Println("Maybe you meant -j or --jump ?")
                 os.Exit(0)
             }
+
+            if arg == "--delete" {
+                os.Args[idx] = "-d"
+                if idx + 1 < len(os.Args) {
+                    parsedArg, convErr := strconv.Atoi(os.Args[idx + 1])
+                    if convErr != nil {
+                        fmt.Println("Error parsing argument : ", convErr)
+                        os.Exit(0)
+                    }
+                    *deleteFlag = parsedArg
+                } else {
+                    fmt.Println("--jump expects an argument <index>")
+                    os.Exit(0)
+                }
+            } else if arg =="-d" {
+                if idx + 1 >= len(os.Args) {
+                    fmt.Printf("%s expects an argument <index>\n", arg)
+                    os.Exit(0)
+                }
+            } else if arg == "--d" {
+                fmt.Println("Maybe you meant -d or --delete ?")
+                os.Exit(0)
+            }
         }
         flag.Parse()
 
@@ -233,7 +331,8 @@ func main () {
             //TODO: Update the command lists
             fmt.Println("Usage: mterm [-i | --insert] <path>")
             fmt.Printf("%6s", "mterm [-p || --print]\n")
-            fmt.Printf("%6s", "mterm [-j || --jump] | mterm\n")
+            fmt.Printf("%6s", "mterm [-j || --jump] <index> | mterm\n")
+            fmt.Printf("%6s", "mterm [-d || --delete] <index>\n")
             os.Exit(0)
         }
 
@@ -245,6 +344,8 @@ func main () {
             printSavedPaths()
         case "-j":
             jumpToPath(*jumpFlag)
+        case "-d":
+            deletePath(*deleteFlag)
 
         default:
             fmt.Println("No such command, run mterm -h for help")
